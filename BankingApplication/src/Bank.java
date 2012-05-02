b8c906acece968fae3f8edf1cc9a784d347fac39
Chris van Es <cvanes@gmail.com>
2012-05-02 12:41:04 +0100

    create account method

--- BankingApplication/src/Bank.java
+++ BankingApplication/src/Bank.java
@@ -5,10 +5,16 @@ public class Bank {
 
     private static Map<String, Account> accounts = new HashMap<String, Account>();
 
+    private static int lastAccountNumber = 0;
+
     public static String createAccount(String name) {
-        String accountNumber = "00000001";
+        String accountNumber = nextAccountNumber();
         accounts.put(accountNumber, new Account(accountNumber, name));
         return accountNumber;
+    }
+
+    private static String nextAccountNumber() {
+        return String.format("%08d", ++lastAccountNumber);
     }
 
 }
--- BankingApplication/test/BankTest.java
+++ BankingApplication/test/BankTest.java
@@ -9,9 +9,10 @@ public class BankTest {
         assertEquals(Bank.createAccount("newCustomer"), "00000001");
     }
 
-//    @Test
-//    public void second_account_created_returns_second_account_number() throws Exception {
-//        Bank.createAccount("newCustomer1");
-//        assertEquals(Bank.createAccount("newCustomer2"), "00000002");
-//    }
+    @Test
+    public void account_number_creation_is_sequential() throws Exception {
+        int accountNumber1 = Integer.valueOf(Bank.createAccount("newCustomer1"));
+        int accountNumber2 = Integer.valueOf(Bank.createAccount("newCustomer2"));
+        assertEquals(++accountNumber1, accountNumber2);
+    }
 }
